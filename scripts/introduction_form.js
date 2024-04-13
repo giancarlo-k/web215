13e445ed116c7ae02043ca85ad4db1a55344271c
Giancarlo Kite <giancarlokite@yahoo.com>
2024-04-13 12:15:31 -0400

    j

--- scripts/introduction_form.js
+++ scripts/introduction_form.js
@@ -17,48 +17,61 @@ submit.onclick = function submit() {
     courses = getElement('fourth'),
     interesting = getElement('interesting');
 
-    if (photo) {
-      let reader = new FileReader();
+  let iceCreamFlavor = document.querySelector('input[name="iceCreamFlavor"]:checked');
+  let selectedFlavor = iceCreamFlavor ? iceCreamFlavor.id.charAt(0).toUpperCase() + iceCreamFlavor.id.slice(1) : 'Not selected';
+
+
+  let programmingLanguages = document.querySelectorAll('input[type="checkbox"]:checked');
+  let selectedLanguages = Array.from(programmingLanguages).map(language => language.id.charAt(0).toUpperCase() + language.id.slice(1)).join(', ');
   
-      reader.onload = function (e) {
-        let photoSrc = e.target.result;
+  if (selectedLanguages === '') {
+    selectedLanguages = 'None selected';
+  }
   
-        document.querySelector('main').innerHTML = `
-          <div class="imageBox">
-            <img class="image" src="${photoSrc}" alt="photo" style="width: 15vh; height:auto; padding-top: 3vh;"><br>
-            <div class="caption">
-              <i>${caption}</i><br>
-            </div><br>
-          </div>
-  
-          <div class="listBox">
-          <ul>
-            <li><b>Personal background: </b>${ personal}</li>
-            <li><b>Professional background: </b> ${ professional}</li>
-            <li><b>Academic background: </b> ${ academic}</li>
-            <li><b>Primary Computer Platform: </b>${ primaryComp}</li>
-            <li><b>Courses I'm Taking & Why:</b>
-    
-            <ul>${(() => {
-              let coursesList = courses.split('\n').map(course => course.trim()).filter(Boolean);
-              let content = '';
-              for (let course of coursesList) {
-                let split = course.split(/\s+-\s+/);
-                content += `<li><strong>${split[0]}</strong> - ${split[1]}</li>`;
-              }
-              return content;
-            })()}</ul>
-            <li><b>Funny/Interesting Item about Yourself: </b>${ interesting}</li>
-    
-          </ul>
+
+  if (photo) {
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+      let photoSrc = e.target.result;
+
+      document.querySelector('main').innerHTML = `
+        <div class="imageBox">
+          <img class="image" src="${photoSrc}" alt="photo" style="width: 15vh; height:auto; padding-top: 3vh;"><br>
+          <div class="caption">
+            <i>${caption}</i><br>
+          </div><br>
         </div>
-        `;
-      };
+
+        <div class="listBox">
+        <ul>
+          <li><b>Personal background: </b>${personal}</li>
+          <li><b>Professional background: </b>${professional}</li>
+          <li><b>Academic background: </b>${academic}</li>
+          <li><b>Primary Computer Platform: </b>${primaryComp}</li>
+          <li><b>Courses I'm Taking & Why:</b>
   
-      reader.readAsDataURL(photo);
-    } else {
-      alert('Please select a photo');
-    }
-  };
+          <ul>${(() => {
+            let coursesList = courses.split('\n').map(course => course.trim()).filter(Boolean);
+            let content = '';
+            for (let course of coursesList) {
+              let split = course.split(/\s+-\s+/);
+              content += `<li><strong>${split[0]}</strong> - ${split[1]}</li>`;
+            }
+            return content;
+          })()}</ul>
+          <li><b>Funny/Interesting Item about Yourself: </b>${interesting}</li>
+          <li><b>Favorite Ice Cream Flavor: </b>${selectedFlavor}</li>
+          <li><b>Favorite Programming Languages: </b>${selectedLanguages}</li>
+        </ul>
+      </div>
+      `;
+    };
+
+    reader.readAsDataURL(photo);
+  } else {
+    alert('Please select a photo');
+  }
+};
 
 
