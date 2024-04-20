7638c9f66bfe17ddee28a4035e0fd5860b888036
Giancarlo Kite <giancarlokite@yahoo.com>
2024-04-20 14:36:44 -0400

    Updated introduction form

--- scripts/introduction_form.js
+++ scripts/introduction_form.js
@@ -38,22 +38,19 @@ function handleSubmit() {
         let courseText = getElement('course' + i);
         let indexOfColon = courseText.indexOf(':');
         if (indexOfColon !== -1) {
-          // Split the course text into two parts: before and after the colon
           let beforeColon = courseText.substring(0, indexOfColon);
           let afterColon = courseText.substring(indexOfColon);
-          // Format the text before the colon to be bold
           courses.push(`<b>${beforeColon}</b>${afterColon}`);
         } else {
-          // If there's no colon, just push the original text
           courses.push(courseText);
         }
       }
 
       document.querySelector('main').innerHTML = `
       <h2>Introduction</h2><br>
-      <div class="mainRectangle">     
+      <form>     
         <div class="imageBox">
-          <img class="image" src="${photoSrc}" alt="photo" style="width: 30vh; height:auto; padding-top: 3vh;"><br>
+          <img class="image" src="${photoSrc}" alt="photo" style="width: auto; height:auto;"><br>
           <div class="caption">
             <i>${caption}</i><br>
           </div><br>
@@ -66,13 +63,15 @@ function handleSubmit() {
             <li><b>Academic background: </b>${academic}</li>
             <li><b>Primary Computer Platform: </b>${primaryComp}</li>
             <li><b>Courses I'm Taking & Why:</b>
-            ${courses.map(course => `<li>${course}</li>`).join('')}
+            <ul>
+              ${courses.map(course => `<li>${course}</li>`).join('')}
+            </ul>
             <li><b>Funny/Interesting Item about Yourself: </b>${interesting}</li>
             <li><b>Favorite Ice Cream Flavor: </b>${selectedFlavor}</li>
             <li><b>Favorite Programming Languages: </b>${selectedLanguages}</li>
           </ul>
         </div>
-      </div>
+      </form>
       `;
     };
 
@@ -97,8 +96,15 @@ function generateTextAreas(numCourses) {
   }
 }
 
+document.getElementById('amountOfCourses').value = 4;
+
 document.getElementById('amountOfCourses').addEventListener('input', function() {
   var amountOfCourses = parseInt(this.value);
   generateTextAreas(amountOfCourses);
 });
+generateTextAreas(4);
 
+document.getElementById('course1').value = "CSC-221 - Advanced Python Programming: Degree Req. + Will teach me all about data retrieval, data manipulation, etc.";
+document.getElementById('course2').value = "WEB-250 - Database Driven Websites: Degree Req. + Will help strengthen my front-end programming skills";
+document.getElementById('course3').value = "CTS-118 - IS Professional Comm.: Degree Req. + Covers all scopes of software development";
+document.getElementById('course4').value = "WEB-215 - Adv. Markup & Scripting: Degree Req. + Might become my main language";
